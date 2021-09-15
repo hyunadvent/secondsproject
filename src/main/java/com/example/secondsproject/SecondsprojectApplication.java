cb732ab86bc4cfa9dd409eaed86e400806c2d43d
idholic <idholic@hancom.com>
2021-09-15 11:12:58 +0900

    Initial commit

---
+++ src/main/java/com/example/secondsproject/SecondsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondsprojectApplication.class, args);
+	}
+
+}
