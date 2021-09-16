32bc716d0813842c0d4c7365e760d9a4e7be0152
idholic <idholic@hancom.com>
2021-09-16 14:03:54 +0900

    상세페이지 보기

--- src/main/java/com/example/secondsproject/controller/ArticleController.java
+++ src/main/java/com/example/secondsproject/controller/ArticleController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -40,6 +41,16 @@ public class ArticleController {
     public String create(ArticleForm form) {
         log.info(form.toString());
         return "redirect:/articles";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) { // url의 {id} 값을 변수화
+        // id를 통해 Article을 가져옴
+        Article article = articleRepository.findById(id).orElse(null);
+
+        // article을 뷰 페이지로 전달
+        model.addAttribute("article", article);
+        return "articles/show";
     }
 
 }
