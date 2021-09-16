7d8dc3e2a17d4b88065914115f421c611ad3ce7c
idholic <idholic@hancom.com>
2021-09-16 13:30:05 +0900

    목록가져오기 구현

--- src/main/java/com/example/secondsproject/controller/ArticleController.java
+++ src/main/java/com/example/secondsproject/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.example.secondsproject.controller;
 
 import com.example.secondsproject.dto.ArticleForm;
+import com.example.secondsproject.entity.Article;
+import com.example.secondsproject.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,13 +11,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
+@RequiredArgsConstructor // final 필드 값을 알아서 가져옴! (@autowired 대체!)
 @Controller
 public class ArticleController {
 
+    // 리파지터리 객체 자동 삽입 됨! 위에서 @RequiredArgsConstructor 했음!
+    private final ArticleRepository articleRepository;
+
     @GetMapping("/articles")
     public String index(Model model) {
+        // 모든 article을 가져옴
+        // Iterable 인터페이스는 ArrayList의 부모 인터페이스
+        Iterable<Article> articleList = articleRepository.findAll();
 
-        model.addAttribute("msg", "글쓰기");
+        // 뷰페이지로 articles를 모델에 넣어 전달.
+        model.addAttribute("articles", articleList);
+
+        // 뷰페이지 설정.
         return "articles/index";
     }
 
@@ -28,4 +41,5 @@ public class ArticleController {
         log.info(form.toString());
         return "redirect:/articles";
     }
+
 }
--- src/main/java/com/example/secondsproject/controller/HelloController.java
+++ src/main/java/com/example/secondsproject/controller/HelloController.java
@@ -1,13 +1,38 @@
 package com.example.secondsproject.controller;
 
+import com.example.secondsproject.entity.Article;
+import com.example.secondsproject.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor // final 필드 값을 알아서 가져옴! (@autowired 대체!)
 public class HelloController {
+    // 리파지터리 객체 자동 삽입 됨! 위에서 @RequiredArgsConstructor 했음!
+    private final ArticleRepository articleRepository;
 
     @GetMapping("/")
     public String hello() {
         return "helloworld";
+    }
+
+    @GetMapping("/init")
+    public String init() {
+        Article temp1 = Article.builder().id(1L).content("AAA").title("제목-A").build();
+        Article temp2 = Article.builder().id(2L).content("BBB").title("제목-BBB").build();
+        Article temp3 = Article.builder().id(3L).content("CCC").title("제목-CCC").build();
+
+        Article saved1 = articleRepository.save(temp1);
+        Article saved2 = articleRepository.save(temp2);
+        Article saved3 = articleRepository.save(temp3);
+
+        log.info(saved1.toString());
+        log.info(saved2.toString());
+        log.info(saved3.toString());
+
+        return "redirect:/articles";
     }
 }
